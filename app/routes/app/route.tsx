2a555609e84cd1cbb80f9bb522645259f8821d3b
Menard Maranan <menardmaranan.codes@gmail.com>
2026-01-25 20:39:22 +0800

    feat: Added History to sidebar menu
    Signed-by: Menard Maranan menardmaranan16@gmail.com

--- app/routes/app/route.tsx
+++ app/routes/app/route.tsx
@@ -20,6 +20,7 @@ export default function App() {
     <AppProvider embedded apiKey={apiKey}>
       <s-app-nav>
         <s-link href="/app">Home</s-link>
+        <s-link href="/app/jobs">History</s-link>
         <s-link href="/app/additional">Additional page</s-link>
         <s-link href="/app/bulk-generate-alt-texts">
           Bulk Generate Alt Texts
